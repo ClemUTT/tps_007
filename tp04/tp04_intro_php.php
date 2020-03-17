427ea8fbe6fc4a36923467154642ba96bc4b6825
clement p <clement.plantier68@gmail.com>
2020-03-17 11:25:41 +0100

    début tp04

---
+++ tp04/tp04_intro_php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="bootstrap.css">
+</head>
+  <body>
+    <div class="container">
+      <div class="panel panel-success">
+        <div class="panel-heading">Exercice 1 : validation de la configuration div-isi</div>
+        <div class="panel-body">
+          <?php echo 'Bonjour à tous !'; ?>
+        </div>
+      </div>
+    </div>
+
+
+  </body>
+</html>
