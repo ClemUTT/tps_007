5ac2bededaa01bece3928ba94bb0b50fb67ab177
ClemUTT <clement.plantier@utt.fr>
2020-06-05 23:42:21 +0200

    proposition 1 docu tp13

--- tp13_mvc1/public/documentation/mesPropositions.php
+++ tp13_mvc1/public/documentation/mesPropositions.php
@@ -9,8 +9,13 @@
     ?>
 
 
-
-
+    <h3>Idées d'améliorations : </h3>
+    <ul>
+        <li>Rendre possible le routage vers des pages qui n'ont pas vocation à être dynamique (c'est à dire des pages "vitrines"). En effet, cette page de documentation par exemple
+        peut avoir vocation à possèder un contenu fixe. Il semble alors ne pas être très utile de créer un fichier controller et model pour chaque page qui contiendrait des données
+        fixes. Pour convenir à cela, il suffirait par exemple de créer un seul controller permettant le routage vers n'importe quelle page dont le chemin est spécifié en paramètre,
+        ou bien d'aggrémenter le switch de conditions pour chaque page.</li>
+    </ul>
 
 
     <?php
